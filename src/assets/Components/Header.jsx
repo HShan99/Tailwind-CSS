23dd4ed00a19ec82bb909fb883f8427000010c89
HShan99 <shanharsharathnayake@gmail.com>
2024-12-30 23:29:00 +0530

    install tailwind

---
+++ src/assets/Components/Header.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Header() {
+  return (
+    <div>
+      <h1 className="text-red-600 text-center text-4xl pt-3">
+        This Is the Header
+      </h1>
+    </div>
+  );
+}
+
+export default Header;
